32da9761146fe3ec0206d6d5eb61b3bcf65871e2
Luiz Ricardo <utluiz@gmail.com>
2014-09-17 12:12:03 -0300

    jagva 8 example

--- random-code/src/main/java/br/com/starcode/comparator/ComparatorExample.java
+++ random-code/src/main/java/br/com/starcode/comparator/ComparatorExample.java
@@ -6,43 +6,51 @@ import java.util.Comparator;
 public class ComparatorExample {
 
 	public static void main(String[] args) {
-	
-	Cliente[] clientes = {
-			new Cliente("Bruno", 30),
-			new Cliente("Maria", 28),
-			new Cliente("Carlos", 40)
-	};
-	
-	//ordenar por nome
-	Arrays.sort(clientes, new Comparator<Cliente>() {
-		@Override
-		public int compare(Cliente b1, Cliente b2) {
-			//TODO testar nulos
-			return b1.getNome().compareTo(b2.getNome()); //delegar para comparador da String
-		}
-	});
-	imprime(clientes);
-	
-	//ordenar por idade
-	Arrays.sort(clientes, new Comparator<Cliente>() {
-		@Override
-		public int compare(Cliente b1, Cliente b2) {
-			//TODO testar nulos
+		
+		Cliente[] clientes = {
+				new Cliente("Bruno", 30),
+				new Cliente("Maria", 28),
+				new Cliente("Carlos", 40)
+		};
+		
+		//ordenar por nome
+		
+		/* Java 8: Arrays.sort(clientes, (Cliente b1, Cliente b2) -> b1.getNome().compareTo(b2.getNome())); */
+		Arrays.sort(clientes, new Comparator<Cliente>() {
+			@Override
+			public int compare(Cliente b1, Cliente b2) {
+				//TODO testar nulos
+				return b1.getNome().compareTo(b2.getNome()); //delegar para comparador da String
+			}
+		});
+		imprime(clientes);
+		
+		//ordenar por idade
+		/* Java 8: 
+		Arrays.sort(clientes, (Cliente b1, Cliente b2) -> {
 			if (b1.getIdade() > b1.getIdade()) return 1;
 			if (b1.getIdade() < b2.getIdade()) return -1;
 			return 0;
+		});  */
+		Arrays.sort(clientes, new Comparator<Cliente>() {
+			@Override
+			public int compare(Cliente b1, Cliente b2) {
+				//TODO testar nulos
+				if (b1.getIdade() > b1.getIdade()) return 1;
+				if (b1.getIdade() < b2.getIdade()) return -1;
+				return 0;
+			}
+		});
+		imprime(clientes);
+			
 		}
-	});
-	imprime(clientes);
 		
-	}
-	
-	private static void imprime(Cliente[] clientes) {
-		for (Cliente cliente : clientes) {
-			System.out.print(cliente.getNome());
-			System.out.print("(" + cliente.getIdade() + "),");
+		private static void imprime(Cliente[] clientes) {
+			for (Cliente cliente : clientes) {
+				System.out.print(cliente.getNome());
+				System.out.print("(" + cliente.getIdade() + "),");
+			}
+			System.out.println();
 		}
-		System.out.println();
-	}
 	
 }
