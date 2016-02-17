4911704103441d683f46e6c98d1a983b5ab87eb4
Luiz Silva <lsilva@atlassian.com>
2016-02-17 13:13:00 +1100

    socket based chat

--- random-code/src/main/java/RedirectSystemOut.java
+++ random-code/src/main/java/RedirectSystemOut.java
@@ -8,37 +8,32 @@ public class RedirectSystemOut {
 
     public static void main(String[] args) {
 
-        PrintStream original = System.out;
-        StringBuilder sb = new StringBuilder();
-        System.setOut(new PrintStream(new OutputStream() {
+    PrintStream original = System.out;
+    StringBuilder sb = new StringBuilder();
+    System.setOut(new PrintStream(new OutputStream() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        @Override
+        public void write(int b) throws IOException {
+            baos.write(b);
+        }
+        @Override
+        public void flush() throws IOException {
+            sb.append(baos.toString());
+            baos.reset();
+        }
+        @Override
+        public void close() throws IOException {
+            sb.append(baos.toString());
+            baos.reset();
+        }
+    }, true));
 
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    System.out.print("Capture ");
+    System.out.println("Isto ");
+    System.out.format("Para %s!", "Mim");
 
-            @Override
-            public void write(int b) throws IOException {
-                baos.write(b);
-            }
-
-            @Override
-            public void flush() throws IOException {
-                sb.append(baos.toString());
-                baos.reset();
-            }
-
-            @Override
-            public void close() throws IOException {
-                sb.append(baos.toString());
-                baos.reset();
-            }
-
-        }, true));
-
-        System.out.print("Capture ");
-        System.out.println("Isto ");
-        System.out.format("Para %s!", "Mim");
-
-        System.setOut(original);
-        System.out.format("Resultado capturado:%n%s", sb);
+    System.setOut(original);
+    System.out.format("Resultado capturado:%n%s", sb);
 
     }
 }
